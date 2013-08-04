eb4026f3c4d2bb88566aceb7839605fb438c2c69
Andreas Jung <yet@gmx.de>
2013-08-04 18:19:50 +0200

    added

---
+++ pyhsgw/hs_find.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+import plac
+
+from hsgw import HomeserverConnection
+
+@plac.annotations(
+key=('Homeserver key', 'option', 'k'),
+name=('Regular expression', 'option', 'r'))
+def finder(key=None, name=None):
+    conn = HomeserverConnection(key=key)
+    for row in conn.findAddrByName(name):
+        print row
+
+def main():
+    plac.call(finder)
