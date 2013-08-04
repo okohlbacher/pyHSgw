af638637a95d91265fa15cd7f0716473e7f7d792
Oliver Kohlbacher <oliver.kohlbacher@uni-tuebingen.de>
2013-08-04 18:36:02 +0200

    added

--- pyhsgw/test_hs_connection.py
+++ pyhsgw/test_hs_connection.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import sys
-from pyhsgw.hsgw import HomeserverConnection
+from hsgw import HomeserverConnection
 
 regex = r'.*Big Bang HELLIGKEIT$'
 conn = HomeserverConnection(key=sys.argv[1])
 bigbang = conn.getAddrByName(regex)
 print "Helligkeit: ", conn.getValue(bigbang)
 print "Current value: ", conn.getValue(bigbang)
-conn.setValue(bigbang, "100.0")
+conn.setValue(bigbang, "0.0")
 print "Helligkeit: ", conn.getValue(bigbang)
